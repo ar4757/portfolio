e483f877f74a8d21bb8175e4fcd5861828fe8a00
Andrew Ratz <ar4757@gmail.com>
2025-05-30 11:01:05 -0400

    Tested fixes

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 function NavBar() {
  return (
     <nav className="nav">
-      <NavLink className="btn btn--notlogo" role="button" to="/resume"><span>Resume</span></NavLink>
-      <NavLink className="btn btn--notlogo" role="button" to="/"><span>Home</span></NavLink>
+      <NavLink className="btn btn--notlogo" role="button" to="/portfolio/resume"><span>Resume</span></NavLink>
+      <NavLink className="btn btn--notlogo" role="button" to="/portfolio"><span>Home</span></NavLink>
     </nav>
  );
 };
