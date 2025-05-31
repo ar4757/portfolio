cee00c1a6ff88db7129ddd3c4ec52e9ceb794c77
Andrew Ratz <ar4757@gmail.com>
2025-05-31 10:50:33 -0400

    Cloudflare pages pt 1

--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -4,7 +4,7 @@ function Resume() {
   return (
     <>
       <br/>
-      <embed className="pdf" src="/portfolio/Andrew_Ratz_Resume.pdf#toolbar=0&navpanes=0&scrollbar=0" type="application/pdf" />
+      <embed className="pdf" src="Andrew_Ratz_Resume.pdf#toolbar=0&navpanes=0&scrollbar=0" type="application/pdf" />
     </>
   )
 }
