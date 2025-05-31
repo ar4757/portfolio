62dcff29f1467c51f093203821d5c73b28bc5b60
Andrew Ratz <ar4757@gmail.com>
2025-05-30 23:03:38 -0400

    Switch to HashRouter for github pages. Weird

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import theme from './theme.ts';
 import { ThemeProvider } from '@mui/material'
 import UserApp from './Pages/UserApp.tsx'
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
+import { HashRouter, Routes, Route, Outlet } from 'react-router-dom'
 import Home from './Pages/Home.tsx';
 import Resume from './Pages/Resume.tsx';
 
@@ -13,15 +13,15 @@ function App() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
-        <Route path="/portfolio/" element={<UserApp/>}>
+        <Route path="/" element={<UserApp/>}>
           <Route path="" element={<Home />} />
-          <Route path="resume" element={<Resume />} />
+          <Route path="/resume" element={<Resume />} />
         </Route>
       </Routes>
       <Outlet/>
-      </BrowserRouter>
+      </HashRouter>
       </ThemeProvider>
     </LocalizationProvider>
   )
