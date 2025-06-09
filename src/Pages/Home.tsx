961d5dfa4ce3fb9c6a6718700ef3c64d32b1d50a
Andrew Ratz <ar4757@gmail.com>
2025-06-09 08:51:32 -0400

    Fix resume and gamepricedrops url

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,7 +4,7 @@ import { PortfolioItem } from '../Types/PortfolioItem';
 
 function Home() {
   const allGames: PortfolioItem[] = [
-    {name: "Game Price Drops", skills: ["React", "Node.js", "HTML", "CSS", "JavaScript", "TypeScript", "PostgreSQL", "AWS"], description: "A website that scrapes the latest video game sales data and notifies users of new sales.", link_url: "https://www.gamepricedrops.com", image_url: "project_imgs/GamePriceDrops.webp", repo_url: null},
+    {name: "Game Price Drops", skills: ["React", "Node.js", "HTML", "CSS", "JavaScript", "TypeScript", "PostgreSQL", "AWS"], description: "A website that scrapes the latest video game sales data and notifies users of new sales.", link_url: "https://gamepricedrops.com", image_url: "project_imgs/GamePriceDrops.webp", repo_url: null},
     {name: "Homer", skills: ["React", "Node.js", "HTML", "CSS", "JavaScript", "TypeScript", "PostgreSQL", "AWS"], description: "A website used by hundreds of homeowners and businesses to find each other.", link_url: null, image_url: "project_imgs/Homer.webp", repo_url: null},
     {name: "Persona 5 Fusion Calculator", skills: ["iOS", "Swift", "Objective C", "Xcode"], description: "An iOS app that identifies Persona fusions for the hit videogame Persona 5.", link_url: null, image_url: "https://github.com/ar4757/Fusion-Calculator/blob/master/GitHub%20Images/demo.gif?raw=true", repo_url: "https://github.com/ar4757/Fusion-Calculator"},
     {name: "Route My Run", skills: ["iOS", "Swift", "Objective C", "Xcode"], description: "An iOS app that recommends running routes, and provides GPS tracking, turn-by-turn directions, and social networking features.", link_url: null, image_url: "project_imgs/RouteMyRun.webp", repo_url: "https://github.com/ar4757/Route-My-Run"},
